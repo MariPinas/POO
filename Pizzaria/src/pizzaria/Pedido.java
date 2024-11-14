33e58a83f2ad7ee17f365fdd347bdccfffd65f28
MariPinas <marianapls2003@gmail.com>
2024-11-13 22:21:52 -0300

    adicionado atributo de pedido

--- Pizzaria/src/pizzaria/Pedido.java
+++ Pizzaria/src/pizzaria/Pedido.java
@@ -1,6 +1,12 @@
 
 package pizzaria;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Pedido {
-    //
+    private int idCliente;
+    private float total;
+    private int idPedido;
+    private List<Pizza>pizzasPedido = new ArrayList();
 }

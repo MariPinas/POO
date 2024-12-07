892b469a9ee30238cd7518e9f196ef4de810fa5b
MariPinas <marianapls2003@gmail.com>
2024-12-07 18:40:35 -0300

    :sparkles: corrigido o bug do arquivo

--- GestaoHospitalar/src/gestaohospitalar/Atendimento.java
+++ GestaoHospitalar/src/gestaohospitalar/Atendimento.java
@@ -5,16 +5,26 @@ package gestaohospitalar;
  * @author aluno
  */
 public class Atendimento {
+    private int id;
     private EstadoAtendimento status;
     private Medico medico;
     private Paciente paciente;
 
-    public Atendimento(EstadoAtendimento status, Medico medico, Paciente paciente) {
+    public Atendimento(int id, EstadoAtendimento status, Medico medico, Paciente paciente) {
+        this.id=id;
         this.status = status;
         this.medico = medico;
         this.paciente = paciente;
     }
+    
+     public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+    
     public EstadoAtendimento getStatus() {
         return status;
     }
@@ -39,5 +49,10 @@ public class Atendimento {
         this.paciente = paciente;
     }
     
-    
+    public void imprimir(){
+        System.out.println("ID Atendimento: " + id);
+        System.out.println("Nome Medico: " + medico.getNome());
+        System.out.println("Nome Paciente: " + paciente.getNome());
+        System.out.println("Status: " + status);
+    }
 }
--- GestaoHospitalar/src/gestaohospitalar/DAOAtendimento.java
+++ GestaoHospitalar/src/gestaohospitalar/DAOAtendimento.java
@@ -4,10 +4,55 @@
  */
 package gestaohospitalar;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author aluno
  */
 public class DAOAtendimento {
-    
+    private List<Atendimento> databaseAtendimentos = new ArrayList();
+
+    public Atendimento create(Atendimento d) {
+        databaseAtendimentos.add(d);
+        return d;
+    }
+
+    public Atendimento read(int id) {
+        for (Atendimento d : databaseAtendimentos) {
+            if (d.getId() == id) {
+                return d;
+            }
+        }
+        return null;
+    }
+
+    public boolean update(Atendimento d) {
+        Atendimento existeP = read(d.getId());
+        if (existeP != null) {
+            existeP.setMedico(d.getMedico());
+            existeP.setPaciente(d.getPaciente());
+            existeP.setStatus(d.getStatus());
+            return true;
+        }
+        return false;
+    }
+
+    public boolean delete(Atendimento d) {
+        Atendimento existeP = read(d.getId());
+        if (existeP != null) {
+            databaseAtendimentos.remove(d);
+            return true;
+        }
+        return false;
+    }
+
+    public void getAll() {
+
+        for (Atendimento i : databaseAtendimentos) {
+            i.imprimir();
+        }
+
+    }
 }

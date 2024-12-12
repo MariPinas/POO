61d03e6d8b4668d9bff451cc11f1c0d3b937b50b
MariPinas <marianapls2003@gmail.com>
2024-12-11 22:04:41 -0300

    :sparkles: 11-12-2024 Execucao paralela com Threads

---
+++ ExecucaoParalela/src/execucaoparalela/ExecucaoParalela.java
@@ -0,0 +1,19 @@
+package execucaoparalela;
+public class ExecucaoParalela {
+    
+    public static void main(String[] args) {
+        Trabalhador a = new Trabalhador();
+        Trabalhador b=new Trabalhador();
+        a.setNome("AA");
+        b.setNome("BB");
+        a.start();
+        b.start();
+        
+        a.setPriority(9);
+        b.setPriority(9);
+        while(true){
+            System.out.println("Principal");
+        }
+    }
+    
+}
---
+++ ExecucaoParalela/src/execucaoparalela/Trabalhador.java
@@ -0,0 +1,19 @@
+package execucaoparalela;
+
+public class Trabalhador extends Thread {
+
+    String nome = "";
+    int i = 0;
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            i++;
+            System.out.println(nome + " " + i);
+        }
+    }
+}

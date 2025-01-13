119e2bc4ea25a05d5914870570b589c2971c3553
MariPinas <marianapls2003@gmail.com>
2025-01-13 15:33:48 -0300

    :sparkles: add classe UsuarioAluno

---
+++ Biblioteca/src/main/java/com/example/demo/UsuarioAluno.java
@@ -0,0 +1,37 @@
+package com.example.demo;
+
+import java.util.Date;
+
+public class UsuarioAluno extends Usuario {
+	private Date dataLimite;
+
+	UsuarioAluno(String st, Date dt) {
+		super(st);
+		dataLimite = dt;
+	}
+
+	public void renovaCartao(Date dt) {
+		dataLimite = dt;
+	}
+
+	public boolean isRegular() {
+		Date hoje = new Date();
+		return (dataLimite.after(hoje));
+	}
+
+	public boolean isARenovar() {
+		return (!isRegular());
+	}
+
+	public int getCotaMaxima() {
+		return (isRegular() ? 3 : super.getCotaMaxima());
+	}
+
+	public int getPrazoMaximo() {
+		return (isRegular() ? 7 : super.getPrazoMaximo());
+	}
+
+	public String toString() {
+		return ("Aluno " + getNome());
+	}
+}
